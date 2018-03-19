0451c59c2d15a62848029d30de832a93cd88f951
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2018-03-19 14:50:35 -0700

    Revert "spark showings stuff"
    This reverts commit f2ffb841a3e33b9179eefba7bb6e551d1b18fcaa.

--- app.js
+++ app.js
@@ -5,37 +5,14 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var index = require('./routes/index');
+var routes = require('./routes/index');
 var users = require('./routes/users');
-var chat = require('./routes/chat');
-
-const passport = require('passport');
-const JwtStrategy = require('passport-jwt').Strategy;
-const ExtractJwt = require('passport-jwt').ExtractJwt;
-const secrets = require('../secrets.js');
-const cache = require('./data/cacheclient.js');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-// passport auth setup
-const jwtOptions = {
-  secretOrKey: secrets.jwt,
-  algorithms: ['HS256']
-};
-jwtOptions.jwtFromRequest = ExtractJwt.fromHeader("token");
-passport.use(new JwtStrategy(jwtOptions, (payload, done) => {
-  cache.getAsync("token:"+payload.id).then(user => {
-    return done(null, JSON.parse(user));
-  }).catch(err => {
-    console.log("AUTHENTIFICATION ERROR!!!");
-    return done(err, false);
-  });
-}
-));
+app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -45,9 +22,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
+app.use('/', routes);
 app.use('/users', users);
-app.use('/chat', chat);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,15 +32,29 @@ app.use(function(req, res, next) {
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// error handlers
 
-  // render the error page
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
